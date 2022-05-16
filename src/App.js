420ec4bd4bb96fee5746ad745a20fd24cf837e4c
Ashley Richard <ashleymrichard@gmail.com>
2022-05-15 22:55:56 -0700

    Starship Map Works

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from "react";
 
 function App() {
+
+  const [starship, setStarship] = useState([])
+
+  const getStarship = async () => {
+    const response = await fetch(`https://swapi.dev/api/starships/`)
+    const data = await response.json()
+    setStarship(data.results)
+  }
+
+  useEffect(() => {
+    getStarship()
+  }, [])
+
+  const allStarships = starship.map((ship, index) => {
+    return (
+      <div key={index}>
+        {ship.name}
+      </div>
+    )
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>STAR WARS STARSHIPS</h1>
+      <section>
+        {allStarships}
+      </section>
     </div>
   );
 }
